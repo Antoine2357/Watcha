ea6d87102ee563ba94d30e82b9b1592fbc643f79
Johan Ceugnart <johan.ceugnart@gmail.com>
2019-04-28 12:49:35 +0200

    Work css in navigation change link and order

--- src/App.js
+++ src/App.js
@@ -44,27 +44,35 @@ class App extends Component {
                     <img className="logo" src="https://image.noelshack.com/fichiers/2019/15/5/1555076151-screenshot-from-2019-04-12-11-21-07-convertimage-1.png" />
                   </NavLink></li>
                 </ul>
+                <div className="link-nav">
                 <ul className="navbar-nav mx-auto">
 
-                  <li><NavLink activeClassName="active" className="nav-link" exact to="/gallery/Adventure/12">Adventure</NavLink></li>
-                  <li><NavLink activeClassName="active" className="nav-link" exact to="/gallery/Science Fiction/878">Science Fiction</NavLink></li>
-                  <li><NavLink activeClassName="active" className="nav-link" exact to="/gallery/Action/28">Action</NavLink></li>
-                  <li><NavLink activeClassName="active" className="nav-link" exact to="/gallery/Crimes/80">Crimes</NavLink></li>
-                  <li><NavLink activeClassName="active" className="nav-link" exact to="/gallery/Western/37">Westerns</NavLink></li>
-                  <li><NavLink activeClassName="active" className="nav-link" exact to="/gallery/Drama/18">Drama</NavLink></li>
-                  <li><NavLink activeClassName="active" className="nav-link" exact to="/gallery/Comedy/35">Comedy</NavLink></li>
-                  <li><NavLink activeClassName="active" className="nav-link" exact to="/gallery/Thriller/53">Thriller</NavLink></li>
-                  <li><NavLink activeClassName="active" className="nav-link" exact to="/gallery/Horror/27">Horror</NavLink></li>
-                  <li><NavLink activeClassName="active" className="nav-link" exact to="/gallery/Animation/16">Animation</NavLink></li>
-                  <li><NavLink activeClassName="active" className="nav-link" exact to="/gallery/Documentary/99">Documentary</NavLink></li>
-                  <li><NavLink activeClassName="active" className="nav-link" exact to="/gallery/War/10752">War</NavLink></li>
-                  <li><NavLink activeClassName="active" className="nav-link" exact to="/gallery/History/36">History</NavLink></li>
-                  <li><NavLink activeClassName="active" className="nav-link" exact to="/gallery/Romance/10749">Romance</NavLink></li>
-                  <li><NavLink activeClassName="active" className="nav-link" exact to="/gallery/Music/10402">Music</NavLink></li>
+                  <li><NavLink activeClassName="active" className="textdeco" exact to="/gallery/Adventure/12">Adventure</NavLink></li>
+                  <li><NavLink activeClassName="active" className="textdeco" exact to="/gallery/Science Fiction/878">Sc. Fiction</NavLink></li>
+                  <li><NavLink activeClassName="active" className="textdeco" exact to="/gallery/Action/28">Action</NavLink></li>
+                  <li><NavLink activeClassName="active" className="textdeco" exact to="/gallery/Crimes/80">Crimes</NavLink></li>
+                  <li><NavLink activeClassName="active" className="textdeco" exact to="/gallery/Western/37">Westerns</NavLink></li>
+                  <li><NavLink activeClassName="active" className="textdeco" exact to="/gallery/Drama/18">Drama</NavLink></li>
+                  <li><NavLink activeClassName="active" className="textdeco" exact to="/gallery/Comedy/35">Comedy</NavLink></li>
+                </ul>
+                <ul className="navbar-nav mx-auto">
+                  <li><NavLink activeClassName="active" className="textdeco" exact to="/gallery/Thriller/53">Thriller</NavLink></li>
+                  <li><NavLink activeClassName="active" className="textdeco" exact to="/gallery/Horror/27">Horror</NavLink></li>
+                  <li><NavLink activeClassName="active" className="textdeco" exact to="/gallery/Animation/16">Animation</NavLink></li>
+                  <li><NavLink activeClassName="active" className="textdeco" exact to="/gallery/Documentary/99">Documentary</NavLink></li>
+                  <li><NavLink activeClassName="active" className="textdeco" exact to="/gallery/War/10752">War</NavLink></li>
+                  <li><NavLink activeClassName="active" className="textdeco" exact to="/gallery/History/36">History</NavLink></li>
+                  <li><NavLink activeClassName="active" className="textdeco" exact to="/gallery/Romance/10749">Romance</NavLink></li>
+                  <li><NavLink activeClassName="active" className="textdeco" exact to="/gallery/Music/10402">Music</NavLink></li>
+                </ul>
+                </div>
+
+                <ul>
                   <li className="nav-item">
+                  
                     <Dropdown nav isOpen={this.state.dropdownOpen} toggle={this.toggle}>
                       <DropdownToggle nav caret>
-                        Listes
+                      Listes
                     </DropdownToggle>
                       <DropdownMenu>
                       <DropdownItem>
